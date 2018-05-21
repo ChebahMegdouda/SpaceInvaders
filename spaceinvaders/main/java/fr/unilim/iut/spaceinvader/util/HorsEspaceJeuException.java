4459c4a3a44b9c3f3e64d742778135b41da33ca0
Lucie Perruchaud <lucie.perruchaud@etu.unilim.fr>
2018-05-21 15:56:16 +0200

    changement de repository

---
+++ spaceinvaders/main/java/fr/unilim/iut/spaceinvader/util/HorsEspaceJeuException.java
@@ -0,0 +1,9 @@
+package fr.unilim.iut.spaceinvader.util;
+
+@SuppressWarnings("serial")
+public class HorsEspaceJeuException extends RuntimeException {
+
+    public HorsEspaceJeuException(String message) {
+	    super(message);
+    }
+}
