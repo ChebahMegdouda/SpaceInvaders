dc13e54f62d8398f3ba8ba2011e11c0ef754bcc7
Megdouda Chebah <megdouda.chebah@etu.unilim.fr>
2018-05-29 14:47:24 +0200

    Debut test débordement vaisseau

---
+++ src/main/java/fr/unilim/iut/SpaceInvaders/util/DebordementEspaceJeuException.java
@@ -0,0 +1,7 @@
+package fr.unilim.iut.SpaceInvaders.util;
+
+public class DebordementEspaceJeuException extends RuntimeException {
+	public DebordementEspaceJeuException(String message) {
+	    super(message);
+    }
+}
